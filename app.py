26f49a7bc347a438572bee60476c5100ddd3a5aa
Ikigai <125873874+ikigai-edu@users.noreply.github.com>
2023-04-03 18:39:59 +0630

    Add files via upload

---
+++ app.py
@@ -0,0 +1,204 @@
+
+import pandas as pd  # pip install pandas openpyxl
+import plotly.express as px  # pip install plotly-express
+import streamlit as st  # pip install streamlit
+
+st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
+
+df = pd.read_excel(
+    io="supermarkt_sales.xlsx",
+    engine="openpyxl",
+    sheet_name="Sales",
+    skiprows=3,
+    usecols="B:R",
+    nrows=1000,
+)
+
+df["hour"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.hour
+
+#st.write(df)
+
+# ---- SIDEBAR ----
+st.sidebar.header("Please Filter Here:")
+city = st.sidebar.multiselect(
+    "Select the City:",
+    options=df["City"].unique(),
+    default=df["City"].unique()
+)
+
+
+customer_type = st.sidebar.multiselect(
+    "Select the Customer Type:",
+    options=df["Customer_type"].unique(),
+    default=df["Customer_type"].unique(),
+)
+
+gender = st.sidebar.multiselect(
+    "Select the Gender:",
+    options=df["Gender"].unique(),
+    default=df["Gender"].unique()
+)
+
+df_selection = df.query(
+    "City == @city & Customer_type ==@customer_type & Gender == @gender"
+)
+
+st.write(df_selection)
+
+
+
+
+# ---- MAINPAGE ----
+st.title(":bar_chart: Sales Dashboard")
+st.markdown("##")
+
+# TOP KPI's
+if df_selection.empty:
+    st.write('DataFrame is empty!')
+    total_sales = 0
+    average_rating = 0
+    star_rating = 0
+    average_sale_by_transaction = 0
+else:
+    total_sales = int(df_selection["Total"].sum())
+    average_rating = round(df_selection["Rating"].mean(), 1)
+    star_rating = ":star:" * int(round(average_rating, 0))
+    average_sale_by_transaction = round(df_selection["Total"].mean(), 2)
+
+left_column, middle_column, right_column = st.columns(3)
+with left_column:
+    st.subheader("Total Sales:")
+    st.subheader(f"US $ {total_sales:,}")
+with middle_column:
+    st.subheader("Average Rating:")
+    st.subheader(f"{average_rating} {star_rating}")
+with right_column:
+    st.subheader("Average Sales Per Transaction:")
+    st.subheader(f"US $ {average_sale_by_transaction}")
+
+st.markdown("""---""")
+
+
+
+
+# SALES BY PRODUCT LINE [BAR CHART]
+sales_by_product_line = (
+    df_selection.groupby(by=["Product line"]).sum()[["Total"]].sort_values(by="Total")
+)
+
+
+fig_product_sales = px.bar(
+    sales_by_product_line,
+    x="Total",
+    y=sales_by_product_line.index,
+    orientation="h",
+    title="<b>Sales by Product Line</b>",
+    #template="plotly_white",
+)
+
+
+# SALES BY HOUR [BAR CHART]
+sales_by_hour = df_selection.groupby(by=["hour"]).sum()[["Total"]]
+fig_hourly_sales = px.bar(
+    sales_by_hour,
+    x=sales_by_hour.index,
+    y="Total",
+    title="<b>Sales by hour</b>",
+    #template="plotly_white",
+)
+
+
+left_column, right_column = st.columns(2)
+left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
+right_column.plotly_chart(fig_product_sales, use_container_width=True)
+
+
+#---- HIDE STREAMLIT STYLE ----
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
